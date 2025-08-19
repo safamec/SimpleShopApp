ab9cc86375492101a782a27fc70dcfe6f1fe4542
Safa Albaiti <safaa3568@gmail.com>
2025-08-19 11:59:50 +0400

    initial commit

---
+++ Data/ShopContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleShopApp.Models;
+
+namespace SimpleShopApp.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
